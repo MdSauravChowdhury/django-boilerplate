0d96bfd47acbe1e5fba9b67fffc565c790b2d051
alex <sauravchowdhury165@gmail.com>
2019-10-04 20:58:45 +0600

    this is basic_web_template ready to django web app intergrated many of libary

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+# from rest_framework import serializers
+
+# from yourApp.models import YourModel
+# from yourApp.models import YourModel
+
+# class QuoteSerializer(serializers.ModelSerializer):
+
+#     class Meta():
+#         model = Quote
+#         fields = ['quote', 'author']
+
+# class QuoteCategorySerializer(serializers.ModelSerializer):
+
+#     class Meta():
+#         model = QuoteCategory
+#         fields = ('__all__')       
+
+#         
---
+++ api/views.py
@@ -0,0 +1,15 @@
+# from rest_framework import generics
+
+# from quotes.models import Quote
+# from quotes.models import QuoteCategory
+
+# from .serializers import serializers_class
+# from .serializers import serializers_class
+
+# class QuoteAPIView(generics.ListAPIView):
+#     queryset = Quote.objects.all()
+#     serializer_class = your_serializers_class
+
+# class QuoteCategoryAPIView(generics.ListAPIView):
+#     queryset = QuoteCategory.objects.all()
+#     serializer_class = your_serializers_class
